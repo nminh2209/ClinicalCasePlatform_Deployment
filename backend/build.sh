ec73b80eaa5bbe70f26c1dfe70548bce6a1959ab
minhnhmindx <nminh22092209@gmail.com>
2025-12-14 15:11:26 +0700

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -9,6 +9,10 @@ echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Setting Django settings module..."
+export DJANGO_SETTINGS_MODULE=clinical_case_platform.settings
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
